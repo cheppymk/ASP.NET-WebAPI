62a81ccdccfac66a9853e4875953b9cf733b17d1
cheppymk <toso.micevski@gmail.com>
2023-08-11 16:58:09 +0200

    HomeWork-Class03
    RestAPI

---
+++ HomeWork-Class03/BookAPI/BookAPI/Controllers/BookController.cs
@@ -0,0 +1,46 @@
+﻿using BookAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+        public ActionResult<IEnumerable<Book>> GetAllBooks()
+        {
+            return Ok(StaticDb.Books);
+        }
+        
+        [HttpGet("byindex")]
+        public ActionResult<Book> GetBookByIndex([FromQuery] int index)
+        {
+            if (index < 0 || index >= StaticDb.Books.Count)
+                return NotFound("Book not found");
+            return Ok(StaticDb.Books[index]);
+        }
+
+        [HttpGet("filter")]
+        public ActionResult<Book> GetBookByAuthorAndTitle([FromQuery] string author, [FromQuery] string title)
+        {
+            var book = StaticDb.Books.FirstOrDefault(b => b.Author == author && b.Title == title);
+            if (book == null) return NotFound("Book not found");
+            return Ok(book);
+        }
+
+        [HttpPost]
+        public ActionResult AddBook([FromBody] Book book)
+        {
+            StaticDb.Books.Add(book);
+            return Ok();
+        }
+
+        [HttpPost("addmany")]
+        public ActionResult<IEnumerable<string>> AddBooks([FromBody] List<Book> books)
+        {
+            StaticDb.Books.AddRange(books);
+            return Ok(books.Select(b => b.Title));
+        }
+    }
+}
---
+++ HomeWork-Class03/BookAPI/BookAPI/StaticDb.cs
@@ -0,0 +1,16 @@
+﻿using BookAPI.Models;
+
+namespace BookAPI
+{
+    public static class StaticDb
+    {
+        public static List<Book> Books = new List<Book>
+    {
+        new Book { Author = "George Orwell", Title = "1984" },
+        new Book { Author = "Aldous Huxley", Title = "Brave New World" },
+        new Book { Author = "Harper Lee", Title = "To Kill a Mockingbird" },
+        new Book { Author = "J.R.R. Tolkien", Title = "The Lord of the Rings" },
+        new Book { Author = "F. Scott Fitzgerald", Title = "The Great Gatsby" }
+    };
+    }
+}
